da6fa4d4e61ed612da1e5a113aab390c2a8f50d9
wildanhkm <wildanhakim98@gmail.com>
2023-02-01 22:19:51 +0700

    init project

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// annotate or explain the shape or type of the variable
+let age = 20;
+// this will cause an error
+// age = 'a'
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// annotate or explain the shape or type of the variable
+let age: number = 20
+
+// this will cause an error
+// age = 'a'
